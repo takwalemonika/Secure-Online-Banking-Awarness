1b21f0e1ded043a4f6ebd61931bec15c0b55fe39
takwalemonika <takwalemonika12@gmail.com>
2026-09-06 17:49:42 +0530

    Add initial service worker setup
    Initialize service worker with install and activate events.

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = "secure-online-banking-v1";
+
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(self.clients.claim());
+});
